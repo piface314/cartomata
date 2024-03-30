15079b1023dcfab18d233539844327b4cccbced5
Henrique Santana <henriquessantana314@gmail.com>
2024-03-29 23:06:05 -0300

    Add background color to template

--- src/template.rs
+++ src/template.rs
@@ -6,8 +6,11 @@ use crate::data::source::DataSourceType;
 use crate::data::Schema;
 use crate::error::{Error, Result};
 
-use serde::Deserialize;
+use regex::Regex;
+use serde::de::{self, Visitor};
+use serde::{Deserialize, Deserializer};
 use std::collections::HashMap;
+use std::fmt;
 use std::fs;
 use std::path::{Path, PathBuf};
 
@@ -26,6 +29,7 @@ pub struct Template {
 pub struct Base {
     pub name: String,
     pub size: CardSize,
+    pub background: HexRgba,
 }
 
 #[derive(Debug, Deserialize)]
@@ -91,5 +95,49 @@ impl Template {
         home.push(".config");
         home.push("cartomata");
         Ok(home)
+    }
+}
+
+#[derive(Debug)]
+pub struct HexRgba(pub ril::pixel::Rgba);
+
+struct HexRgbaVisitor;
+
+impl<'de> Visitor<'de> for HexRgbaVisitor {
+    type Value = HexRgba;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string in the form #RRGGBBAA or #RRGGBB")
+    }
+
+    fn visit_str<E>(self, v: &str) -> std::result::Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        let re =
+            Regex::new(r"^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})?$")
+                .unwrap();
+
+        let captures = re.captures(v).ok_or(E::custom(format!(
+            "string not in form #RRGGBBAA or #RRGGBB: {v}"
+        )))?;
+        let mut values = captures
+            .iter()
+            .skip(1)
+            .map(|c| c.map(|v| u8::from_str_radix(v.as_str(), 16).unwrap()));
+        let r = values.next().unwrap().unwrap_or(0);
+        let g = values.next().unwrap().unwrap_or(0);
+        let b = values.next().unwrap().unwrap_or(0);
+        let a = values.next().unwrap().unwrap_or(255);
+        Ok(HexRgba(ril::pixel::Rgba { r, g, b, a }))
+    }
+}
+
+impl<'de> Deserialize<'de> for HexRgba {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<HexRgba, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(HexRgbaVisitor)
     }
 }
