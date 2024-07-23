a48af420d945adb7bccb6cd516f1d72a7fbbaf3a
Henrique Santana <henriquessantana314@gmail.com>
2024-07-23 00:42:11 -0300

    Add stroke effect

---
+++ examples/cli.rs
@@ -0,0 +1,91 @@
+use cartomata::cli::Cli;
+use cartomata::data::{Card, DynCard};
+use cartomata::decode::Decoder;
+use cartomata::decode::dynamic::DynamicDecoder;
+use cartomata::image::ImgBackend;
+use cartomata::template::Template;
+use cartomata::text::FontManager;
+use clap::Parser;
+use mlua::Lua;
+use serde::Deserialize;
+use std::fs::File;
+
+#[derive(Debug, Deserialize, Card)]
+pub struct SampleCard {
+    pub id: u64,
+    pub name: String,
+    pub level: i64,
+    pub element: i64,
+    pub r#type: i64,
+    pub power: i64,
+    pub size: i64,
+    pub rarity: i64,
+}
+
+fn main() {
+    std::panic::set_hook(Box::new(|panic_info| {
+        if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+            eprintln!("{s}");
+        } else {
+            eprintln!("{panic_info}");
+        }
+    }));
+    let vips = libvips::VipsApp::new("libvips", false).expect("Cannot initialize libvips");
+    let cli = Cli::parse();
+    let template = Template::find(cli.template).unwrap_or_else(|e| panic!("{e}"));
+
+    let fc = fontconfig::Fontconfig::new().unwrap();
+    let mut fm = FontManager::new(&fc);
+    fm.load_from_template(&template).unwrap_or_else(|e| panic!("{e}"));
+    let ib = ImgBackend::new(fm);
+
+    let source_type = cli
+        .source
+        .or(template.source.default)
+        .expect("Choose a data source");
+    let mut source = source_type
+        .open::<DynCard>(&template, &cli.input)
+        .unwrap_or_else(|e| panic!("{e}"));
+    let cards = source.fetch(&cli.ids);
+    let lua = Lua::new();
+    let decoder = DynamicDecoder::new(&lua, &template).unwrap_or_else(|e| panic!("{e}"));
+    for card_res in cards.into_iter() {
+        let card = match card_res {
+            Ok(card) => card,
+            Err(e) => {
+                eprintln!("Warning: {e}");
+                continue;
+            }
+        };
+        let id = card.id();
+        let stack = match decoder.decode(card) {
+            Ok(stack) => stack,
+            Err(e) => {
+                eprintln!("Warning: {e}");
+                continue;
+            }
+        };
+        let image_res = stack.render(&template, &ib);
+        match image_res {
+            Ok(image) => {
+                let mut path = cli.output.clone();
+                path.push(format!("{id}.png"));
+                let mut out_file = match File::create(path) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        eprintln!("Warning: {e}");
+                        continue;
+                    }
+                };
+                image
+                    .write_to_png(&mut out_file)
+                    .unwrap_or_else(|e| eprintln!("Warning: {e}"));
+            }
+            Err(e) => {
+                eprintln!("Warning: {e}");
+                vips.error_buffer().and_then(|e| Ok(println!("{e}"))).unwrap_or_else(|_| {});
+            },
+        }
+    }
+}
+
--- src/image.rs
+++ src/image.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::path::Path;
 
 use cairo::{Context, Format, ImageSurface};
-use libvips::{ops, ops::BandFormat, ops::ResizeOptions, VipsImage};
+use libvips::{ops, VipsImage};
 #[cfg(feature = "cli")]
 use serde::{Deserialize, Serialize};
 
@@ -67,8 +67,19 @@ impl<'f> ImgBackend<'f> {
         let data = img
             .data()
             .map_err(|_| Error::ImageConversionError("cairo", "vips"))?;
-        VipsImage::new_from_memory(&data, w, h, 4, BandFormat::Uchar)
-            .map_err(|_| Error::ImageConversionError("cairo", "vips"))
+        let img = VipsImage::new_from_memory(&data, w, h, 4, ops::BandFormat::Uchar)
+            .map_err(|_| Error::ImageConversionError("cairo", "vips"))?;
+        ops::copy_with_opts(
+            &img,
+            &ops::CopyOptions {
+                interpretation: ops::Interpretation::Srgb,
+                width: img.get_width(),
+                height: img.get_height(),
+                bands: img.get_bands(),
+                ..Default::default()
+            },
+        )
+        .map_err(|_| Error::ImageConversionError("cairo", "vips"))
     }
 
     pub fn load_image(&self, fp: impl AsRef<Path>) -> Result<ImageSurface> {
@@ -140,13 +151,15 @@ impl<'f> ImgBackend<'f> {
         cr.translate(x, y);
         let (sinr, cosr) = r.sin_cos();
         let (ox, oy) = (ox - (ox * cosr - oy * sinr), oy - (ox * sinr + oy * cosr));
-        cr.translate(ox, oy);
-        cr.rotate(r);
-        if let Some(stroke) = stroke {
-            let img = self.stroked(img, sx, sy, stroke)?;
+        if let Some(Stroke(size, color)) = stroke {
+            cr.translate(ox - size as f64, oy - size as f64);
+            cr.rotate(r);
+            let img = self.stroked(img, sx, sy, size, color)?;
             cr.set_source_surface(img, 0.0, 0.0)
                 .map_err(|e| Error::CairoError(e.to_string()))?;
         } else {
+            cr.translate(ox, oy);
+            cr.rotate(r);
             cr.scale(sx, sy);
             cr.set_source_surface(img, 0.0, 0.0)
                 .map_err(|e| Error::CairoError(e.to_string()))?;
@@ -161,16 +174,69 @@ impl<'f> ImgBackend<'f> {
         img: &mut ImageSurface,
         sx: f64,
         sy: f64,
-        stroke: Stroke,
+        radius: i32,
+        color: Color,
     ) -> Result<ImageSurface> {
         let img = Self::cairo_to_vips(img)?;
-        let opts = ResizeOptions {
-            vscale: sy,
-            ..ResizeOptions::default()
-        };
-        let img =
-            ops::resize_with_opts(&img, sx, &opts).map_err(|e| Error::VipsError(e.to_string()))?;
-        let Stroke(size, color) = stroke;
+        let err = |e: libvips::error::Error| Error::VipsError(e.to_string());
+
+        let mask = ops::black(radius * 2 + 1, radius * 2 + 1).map_err(err)?;
+        let mask = ops::add(
+            &mask,
+            &VipsImage::new_from_image1(&mask, 128.0).map_err(err)?,
+        )
+        .map_err(err)?;
+        ops::draw_circle_with_opts(
+            &mask,
+            &mut [255.0],
+            radius,
+            radius,
+            radius,
+            &ops::DrawCircleOptions { fill: true },
+        )
+        .map_err(err)?;
+
+        let img = ops::resize_with_opts(
+            &img,
+            sx,
+            &ops::ResizeOptions {
+                vscale: sy,
+                ..Default::default()
+            },
+        )
+        .map_err(err)?;
+        let (w, h) = (img.get_width(), img.get_height());
+        let img = ops::embed(&img, radius, radius, w + 2 * radius, h + 2 * radius).map_err(err)?;
+
+        let alpha = ops::extract_band(&img, 3).map_err(err)?;
+        let alpha = ops::morph(&alpha, &mask, ops::OperationMorphology::Dilate).map_err(err)?;
+        let alpha = ops::gaussblur_with_opts(
+            &alpha,
+            0.5,
+            &ops::GaussblurOptions {
+                min_ampl: 0.2,
+                ..Default::default()
+            },
+        )
+        .map_err(err)?;
+
+        let (r, g, b) = color.rgb();
+        let r = VipsImage::new_from_image1(&alpha, r * 255.0).map_err(err)?;
+        let g = VipsImage::new_from_image1(&alpha, g * 255.0).map_err(err)?;
+        let b = VipsImage::new_from_image1(&alpha, b * 255.0).map_err(err)?;
+        let stroke = ops::bandjoin(&mut [r, g, b, alpha]).map_err(err)?;
+        let stroke = ops::copy_with_opts(
+            &stroke,
+            &ops::CopyOptions {
+                interpretation: ops::Interpretation::Srgb,
+                width: stroke.get_width(),
+                height: stroke.get_height(),
+                bands: stroke.get_bands(),
+                ..Default::default()
+            },
+        )
+        .map_err(err)?;
+        let img = ops::composite_2(&stroke, &img, ops::BlendMode::Over).map_err(err)?;
         Self::vips_to_cairo(&img)
     }
 }
